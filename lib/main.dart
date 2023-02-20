559d84baf64f7649afb7b533921cbdbb1ce9eed5
Irene Alonso <i.alonso@accenture.com>
2023-02-20 19:58:54 +0100

    Add style for colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/meals_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,9 +12,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+              .copyWith(secondary: Colors.amber),
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: "Raleway",
+          textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText1:
+                    const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                bodyText2:
+                    const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                headline6: const TextStyle(
+                    fontSize: 20,
+                    fontFamily: "RobotoCondensed",
+                    fontWeight: FontWeight.bold),
+              )),
       home: const CategoriesScreen(),
+      routes: {"/meals": (ctx) => const MealsScreen()},
     );
   }
 }
